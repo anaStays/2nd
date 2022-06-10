7558b788a773271532d3a6385ad73d6181762788
Nastya <an_vorotilina@mail.ru>
2022-06-11 02:48:13 +0300

    final commit with tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
     @Test
     void checkBraces() {
-        Calculator calc=new Calculator();
-        String actual = calc.check("(2+2)+(3+1+(2-(1");
+        Calculations calc=new Calculations();
+        String actual = StringProcessor.check("(2+2)+(3+1+(2-(1");
         String expected = "Incorrect amount of braces\n";
 
         assertEquals(actual,expected);
@@ -15,8 +15,8 @@ class CalculatorTest {
 
     @Test
     void checkOK() {
-        Calculator calc=new Calculator();
-        String actual = calc.check("(2+2)+(3+1)+(2)-(1)");
+        Calculations calc=new Calculations();
+        String actual = StringProcessor.check("(2+2)+(3+1)+(2)-(1)");
         String expected = "OK";
 
         assertEquals(actual,expected);
@@ -24,7 +24,7 @@ class CalculatorTest {
 
     @Test
     void calculationNoVars() {
-        Calculator calc = new Calculator();
+        Calculations calc = new Calculations();
         Scanner scan = new Scanner("");
         float actual = calc.calculation("(2+2)^2+4.5/0.9*2-1",scan);
         float expected = 25.0f;
@@ -34,7 +34,7 @@ class CalculatorTest {
 
     @Test
     void calculationWithVars() {
-        Calculator calc = new Calculator();
+        Calculations calc = new Calculations();
         Scanner scan = new Scanner("1,5");
         float actual = calc.calculation("x^2+ x-3",scan);
         float expected = 0.75f;
